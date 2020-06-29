fc9e353f246074f58076ff727e8ad71392ddf14f
AnirudhTaneja <Anirudhtaneja@live.com>
2020-06-29 15:40:26 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "wNHwfbnO676B2sKWrNAYiJh0G", //process.env.APPLICATION_CONSUMER_KEY_HERE,
-    consumer_secret: "PaxGd5IEbqoWmFhYS6HJTEHKj9mKG4PY8RgoirD8mg60SRvt2W",//process.env.APPLICATION_CONSUMER_SECRET_HERE,
-    access_token: "1008398994404495360-lgkM7JntMo1NHC8f7Ub0lBp2SHgcMF", //process.env.ACCESS_TOKEN_HERE,
-    access_token_secret:  "tm7k2wfmRQwLRRAAJCyqYpoh8Tn1lQcO1PWYcEQQ4WUe1" //process.env.ACCESS_TOKEN_SECRET_HERE
-}
